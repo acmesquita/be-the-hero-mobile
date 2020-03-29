760370b698129d30524bffdfc49cf9dfc74f24eb
Catharina Mesquita <catha.ana.1994@gmail.com>
2020-03-29 12:44:08 -0300

    Criando estrutura das páginas da aplicação

--- App.js
+++ App.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Olá Mundo!</Text>
-      <Text style={styles.text}>Catharina Mesquita</Text>
+    <View>
+      <Text>Olá Mundo!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontSize: 24,
-    textAlign: 'center',
-    fontWeight: 'bold'
-  }
-});
